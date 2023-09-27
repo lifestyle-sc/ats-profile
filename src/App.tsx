a492844f73db47a130775877cd56bd85888693ba
Oluwakayode Momoh <kayodemmh@gmail.com>
2023-09-27 16:19:56 +0100

    adding mock data and public resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './mocks/user_data.json';
 
 function App() {
+  const getData = () => {
+    console.log(data.candidates);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world.</h1>
+      <button onClick={getData}>Click Me</button>
     </div>
   );
 }
